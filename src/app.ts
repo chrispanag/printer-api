982643afa9cbb634d41ef2faf3fa058739495b54
Christos Panagiotakopoulos <chrispanag@gmail.com>
2020-09-15 15:18:04 +0300

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import logger from 'koa-logger';
 import router from './controllers/router';
+import cors from '@koa/cors';
 
 if (process.platform === 'win32') {
     throw 'Not yet supported for win32'
@@ -9,6 +10,7 @@ if (process.platform === 'win32') {
 const app = new Koa();
 
 app.use(logger());
+app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
 export default app;
